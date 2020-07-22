c8e23dab200fc5ad48caadf924963448ff91a22c
Liam Baker <liam.baker2310@gmail.com>
2020-07-22 10:38:31 +0900

    Made post model and added columns to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_015030) do
+ActiveRecord::Schema.define(version: 2020_07_22_012127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "date"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +33,11 @@ ActiveRecord::Schema.define(version: 2020_07_14_015030) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "second_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database"
+Post.delete_all
+User.delete_all
+puts "Cleaned"
+
+puts "Creating users"
+params = {}
+params[:first_name] = "Liam"
+params[:second_name] = "Baker"
+params[:password] = "password"
+params[:email] = "liam.baker@journal.com"
+new_user = User.new(params)
+new_user.save
+puts "Created user #{new_user.id}"
+
+params = {}
+params[:first_name] = "John"
+params[:second_name] = "Doe"
+params[:password] = "password"
+params[:email] = "john.doe@journal.com"
+new_user = User.new(params)
+new_user.save
+puts "Created user #{new_user.id}"
+
+puts "Users created!"
+
+puts "Creating posts"
+10.times do
+  params = {}
+  params[:title] = Faker::GreekPhilosophers.quote
+  params[:content] = Faker::Hacker.say_something_smart
+  params[:date] = Date.today.to_formatted_s(:long)
+  params[:user] = User.all.sample
+  new_post = Post.new(params)
+  new_post.save
+  puts "Created post #{new_post.id}"
+end
+puts "Posts created"
+puts "Finished seeding"
