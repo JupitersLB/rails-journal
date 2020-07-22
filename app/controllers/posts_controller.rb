e1feb6a6f47146324e203b35f09dd0f17daebab5
Liam Baker <liam.baker2310@gmail.com>
2020-07-22 11:01:31 +0900

    posts have index and create post pages

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.where(user: current_user)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :date)
+  end
+end
