77116fdd552f3a744739b54e4e308730845415d3
Liam Baker <liam.baker2310@gmail.com>
2020-07-30 11:43:25 +0900

    Comments added to posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  has_many :comments
 
   # friend requests
   has_many :friend_requests_as_requestor,
