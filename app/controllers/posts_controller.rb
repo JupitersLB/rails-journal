1f0f324cf55aab6b6d6f614410e67ce37bb891b0
Liam Baker <liam.baker2310@gmail.com>
2020-07-30 15:01:21 +0900

    Changed timezone for user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.date = DateTime.now
     @post.user = current_user
     if @post.save
       redirect_to posts_path
@@ -26,6 +27,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :date)
+    params.require(:post).permit(:title, :content)
   end
 end
