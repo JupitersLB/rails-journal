a59422ad129f50982de302f5f2beb9424c6ca350
Liam Baker <liam.baker2310@gmail.com>
2020-07-30 11:24:32 +0900

    made user and friend profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
-  get '/my-page', to: 'users#show', as: 'my_page'
+  get '/:username', to: 'users#show', as: 'user'
 end
