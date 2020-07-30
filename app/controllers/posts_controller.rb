e24ae92188adb02d0b642e09071418c79b5ebb60
Liam Baker <liam.baker2310@gmail.com>
2020-07-30 16:21:46 +0900

    Added avatar images to users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :destroy]
+
   def index
     @my_posts = Post.where(user: current_user)
     ids = current_user.friends.pluck(:id) << current_user.id
@@ -9,9 +11,7 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def create
     @post = Post.new(post_params)
@@ -24,7 +24,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content)
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :comments
+  has_one_attached :photo
 
   # friend requests
   has_many :friend_requests_as_requestor,
