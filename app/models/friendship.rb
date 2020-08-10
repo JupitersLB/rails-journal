7ecdce1f47220116771154c1d0d5035ebd681f16
Liam Baker <liam.baker2310@gmail.com>
2020-08-10 16:44:29 +0900

    Added validations for posts, comments, and friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,12 @@
 class Friendship < ApplicationRecord
   belongs_to :friend_a, class_name: :User
   belongs_to :friend_b, class_name: :User
+  validate :users_are_not_already_friends
+
+  def users_are_not_already_friends
+    if Friendship.where(friend_a_id: friend_b.id, friend_b_id: friend_a.id).exists? ||
+       Friendship.where(friend_a_id: friend_a.id, friend_b_id: friend_b.id).exists?
+      errors.add(:friend_a_id, 'Already friends!')
+    end
+  end
 end
