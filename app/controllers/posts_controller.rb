b03207892f29695679c311d55524d0c2dcc8338b
Liam Baker <liam.baker2310@gmail.com>
2020-08-10 13:50:38 +0900

    made policies for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,23 +2,26 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :destroy]
 
   def index
-    @my_posts = Post.where(user: current_user)
+    @my_posts = policy_scope(Post).where(user: current_user)
     ids = current_user.friends.pluck(:id) << current_user.id
-    @all_posts = Post.where(user_id: ids)
+    @all_posts = policy_scope(Post).where(user_id: ids)
   end
 
   def new
     @post = Post.new
+    authorize @post
   end
 
   def show
     @comment = Comment.new
+    authorize @post
   end
 
   def create
     @post = Post.new(post_params)
     @post.date = DateTime.now
     @post.user = current_user
+    authorize @post
     if @post.save
       redirect_to posts_path
     else
@@ -28,6 +31,7 @@ class PostsController < ApplicationController
 
   def destroy
     @post.destroy
+    authorize @post
     redirect_to posts_path
   end
 
