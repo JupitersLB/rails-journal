d831ea68b2634d338878c5e7ce8e1bf0005d7998
Liam Baker <liam.baker2310@gmail.com>
2020-09-03 15:57:35 +0900

    chatroom and messages without action cable working

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :chatrooms, only: :show
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
 
 end
