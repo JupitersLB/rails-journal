66f711a36eb98c01322eefc9954454f72643fc7a
Liam Baker <liam.baker2310@gmail.com>
2020-11-09 13:25:47 +0900

    tidied style

--- app/controllers/api/v1/chatrooms_controller.rb
+++ app/controllers/api/v1/chatrooms_controller.rb
@@ -1,26 +1,15 @@
 class Api::V1::ChatroomsController < ApplicationController
   def index
     @chatrooms = policy_scope(Chatroom)
-                  .where(receiver: current_user).or(Chatroom.where(sender: current_user))
-                  .includes(:sender, :receiver)
+                 .where(receiver: current_user).or(Chatroom.where(sender: current_user))
+                 .includes(:sender, :receiver)
   end
 
   def show
     @messages = policy_scope(Message)
-               .where(chatroom: params[:id])
-               .includes(:user)
-               # .map do |m|
-               #   {
-               #     id: m.id,
-               #     content: m.content,
-               #     chatroom: m.chatroom_id,
-               #     username: m.user.username,
-               #     created_at: m.created_at
-               #   }
-               # end
+                .where(chatroom: params[:id])
+                .includes(:user)
     message = Message.new
-
-    # render json: [messages]
     authorize message
   end
 end
