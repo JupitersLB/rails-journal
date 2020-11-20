e2d75a831d54fb0f16eeb423a1676490e7922556
Liam Baker <liam.baker2310@gmail.com>
2020-11-20 15:18:36 +0900

    nesting logic made

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,16 @@
 class CommentsController < ApplicationController
+  before_action :find_commentable
+
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @commentable.comments.new(comment_params)
     @comment.user = current_user
-    @comment.post = Post.find(params[:post_id])
     authorize @comment
     if @comment.save
-      unless @comment.post.user == current_user
+      unless @comment.commentable.user == current_user
         Notification.create(recipient: @comment.post.user, actor: current_user, action: "commented", notifiable: @comment.post)
       end
-      @comment.post.comments.each { |comment| Notification.create(recipient: comment.user, actor: current_user, action: "commented", notifiable: @comment.post) if comment.user != current_user}
-      redirect_to post_path(Post.find(params[:post_id]))
+      @comment.commentable.comments.each { |comment| Notification.create(recipient: comment.user, actor: current_user, action: "commented", notifiable: @comment.post) if comment.user != current_user}
+      redirect_to post_path(Comment.find_post(@comment))
     else
       render template: "posts/show"
     end
@@ -17,14 +18,20 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment = Comment.find(params[:id])
+    post = Comment.find_post(@comment)
     @comment.destroy
     authorize @comment
-    redirect_to post_path(Post.find(params[:post_id]))
+    redirect_to post_path(post)
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:note)
+  end
+
+  def find_commentable
+    @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+    @commentable = Post.find_by_id(params[:post_id]) if params[:post_id]
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,15 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :post
+  belongs_to :commentable, polymorphic: true
+  has_many :comments, as: :commentable
   validates :note, presence: true, uniqueness: true
+
+  def self.find_post(ele)
+    if ele.commentable_type == 'Post'
+      Post.find(ele.commentable_id)
+    else
+      c = Comment.find(ele.commentable_id)
+      find_post(c)
+    end
+  end
 end
