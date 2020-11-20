051224a915ac6b4a7fb04446917913ed624f82e0
Liam Baker <liam.baker2310@gmail.com>
2020-11-20 11:44:22 +0900

    added mailer for new user registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_091523) do
+ActiveRecord::Schema.define(version: 2020_11_20_021837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 2020_09_03_091523) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.datetime "last_seen_at"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
