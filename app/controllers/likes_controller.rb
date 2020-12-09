76c0ea0b246a85e8617c500cf9f965b6012447f7
Liam Baker <liam.baker2310@gmail.com>
2020-12-09 15:08:56 +0900

    creating like in controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,16 @@
 class LikesController < ApplicationController
   def create
     @like = Like.new(like_params)
-    if @like.save
-      flash[:success] = 'Thanks for liking!'
-    else
-      flash[:alert] = @like.errors.full_messages.join(', ')
+    @like.user = current_user
+    @like.likeable = @like.post
+    authorize @like
+    @like.save
+    raise
   end
 
   private
 
   def like_params
-    params.require(:like).permit(:value, :post_id)
+    params.permit(:value, :post_id, :likeable_type, :likeable_id)
   end
 end
